d9be2e3b259b9ac52ccf621a0578dedb99f404ac
unknown <bagrat.injgia24@gmail.com>
2023-08-14 21:17:01 +0400

    done

---
+++ WhatsAppTemplate/screens/MainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>Main Screen - WhatsApp UI Replication</Text>
+      {/* Add your WhatsApp UI components here */}
+    </View>
+  );
+};
+
+export default MainScreen;
